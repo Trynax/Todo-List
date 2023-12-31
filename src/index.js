3f85d3787898ff1d203e9eb4ce173a39a3c05106
Trynax <Kandoso111@gmail.com>
2023-12-31 05:07:27 +0100

    Set up css loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// Your JavaScript logic for the todolist
-// Example:
-console.log('Hello from index.js!');
+import './styles.css';
+
+console.log("Hello World")
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
